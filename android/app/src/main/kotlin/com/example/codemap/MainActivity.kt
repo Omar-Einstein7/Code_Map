ffb4d1f13bf456c9e4ade7fbaa197d32e87c3b94
Omar_Ahmed20 <109938759+Omar-Einstein7@users.noreply.github.com>
2025-07-01 18:21:48 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/codemap/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.codemap
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/service_locator.dart
@@ -0,0 +1,19 @@
+import 'package:get_it/get_it.dart';
+
+final GetIt sl = GetIt.instance;
+
+class ServiceLocator {
+  static Future<void> init() async {
+    // Register services here
+    
+    // Core
+    
+    // Data sources
+    
+    // Repositories
+    
+    // Use cases
+    
+    // BLoC / Cubit
+  }
+}
